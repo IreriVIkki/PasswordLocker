2eba33c9992cc707b65f5eb6cc4f21d22b7c0bda
IreriVikki <wambsviki@gmail.com>
2018-08-23 18:45:39 +0300

    initialized an account class

--- accounts.py
+++ accounts.py
@@ -2,4 +2,10 @@ from users import User
 
 
 class Account:
-    pass
+    user_accounts = []
+
+    def __init__(self, account_name, account_url, email, password):
+        self.account_name = account_name
+        self.account_url = account_url
+        self.email = email
+        self.password = password
