4a2418f5695c020f6bb16af94ad486fccf2c6365
IreriVikki <wambsviki@gmail.com>
2018-08-23 19:10:35 +0300

    added a method to add a new account and asserted that it works

--- accounts_test.py
+++ accounts_test.py
@@ -4,4 +4,14 @@ from accounts import Account
 
 
 class AccountsTest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.new_account = Account(
+            'Instagram', 'https://www.instagram.com/', 'wambsviki@gmail.com', '12345')
+
+    def test_init(self):
+        self.assertEqual(self.new_account.account_name, 'Instagram')
+        self.assertEqual(self.new_account.account_url,
+                         'https://www.instagram.com/')
+        self.assertEqual(self.new_account.email, 'wambsviki@gmail.com')
+        self.assertEqual(self.new_account.paa, '12345')
