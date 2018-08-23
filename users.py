d90cd4f0a4e8c97c3fd4244fbbdf48da4e09067a
IreriVikki <wambsviki@gmail.com>
2018-08-23 18:28:29 +0300

    initialized a user class constractor

--- users.py
+++ users.py
@@ -1,2 +1,9 @@
 class User:
     users_list = []
+
+    def __init__(self, firs_name, last_name, email, password, accounts):
+        self.firs_name = firs_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.accounts = accounts
