d2881d80564145f39612a908b21719314d6c4802
IreriVikki <wambsviki@gmail.com>
2018-08-23 18:29:23 +0300

    initialized a user class constractr

--- users_test.py
+++ users_test.py
@@ -1,2 +1,11 @@
 import unittest  # importing the unitttest module
 from users import User  # importing the users class
+
+
+class TestUsers (unittest.TestCase):
+    # this class defines test cases for user class behaviours
+    # TestCase class helps in creating testcases
+
+    def setUp(self):
+        self.new_user = User(
+            'Victor', 'Ireri', 'wambsviki@gmail.com', 'akisijui', [])
